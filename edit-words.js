c6b2b7cb75cd8196868ac96ded661fb89c63f762
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2020-07-31 07:56:56 +0900

    with '#' will be ignored, and an empty message aborts the commit. added ability to insert edited set into database

--- edit-words.js
+++ edit-words.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const db = require('./db');
+const checkAuthenticated = require('./checkAuthenticated');
 
 const editWords = new express.Router();
 
 let userSets = null;
 
-editWords.get('/', (req, res) => {
+editWords.get('/', checkAuthenticated, (req, res) => {
   console.log(req.body);
   db.findOne({ _id: req.user._id }, (err, doc) => {
     if (!doc.myDictionaries || doc.myDictionaries.length === 0) {
@@ -19,7 +20,7 @@ editWords.get('/', (req, res) => {
   });
 });
 
-editWords.get('/:set_name', (req, res) => {
+editWords.get('/:set_name', checkAuthenticated, (req, res) => {
   console.log(req.params.set_name);
   console.log('userSets: ');
   console.log(userSets);
@@ -33,10 +34,33 @@ editWords.get('/:set_name', (req, res) => {
   }
 });
 
-editWords.put('/', (req, res) => {
+editWords.put('/', checkAuthenticated, (req, res) => {
   console.log('hey everybody! yo');
   console.log(req.body);
   res.send({ message: 'received' });
+  let myDictionaries;
+  let title = req.body.title;
+  let newDictionary = req.body.wordList;
+
+  db.findOne({ _id: req.user._id }, (err, doc) => {
+    console.log(doc);
+    myDictionaries = doc.myDictionaries;
+    for (let i = 0; i < myDictionaries.length; i++) {
+      if (myDictionaries[i].title === title) {
+        myDictionaries[i].words = newDictionary;
+      }
+    }
+
+    console.log(myDictionaries);
+
+    db.update({ _id: req.user._id }, { $set: { myDictionaries: myDictionaries } }, {}, (err, numReplaced) => {
+      console.log('yo i found it thjis is the stuff ');
+      if (err) {
+        console.log(err);
+      }
+      console.log(numReplaced);
+    });
+  });
 });
 
 module.exports = editWords;
