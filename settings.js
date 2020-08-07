09505643d2ecd2d88ebed1ecfb7ccc2f6fc8f86d
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2020-08-08 00:02:12 +0900

    fixed broken link

--- settings.js
+++ settings.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const checkAuthenticated = require('./checkAuthenticated');
 
 const settings = new express.Router();
 
 module.exports = settings;
 
-settings.get('/', (req, res) => {
+settings.get('/', checkAuthenticated, (req, res) => {
   res.render('settings', {
     userInfo: req.user,
     page: 'settings'
