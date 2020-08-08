3293fa0ca35554cc9a13bc8022c44909c3018fb8
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2020-08-08 10:12:56 +0900

    finished user settings

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const checkAuthenticated = require('./checkAuthenticated');
+const db = require('./db');
 
 const settings = new express.Router();
 
@@ -10,4 +11,24 @@ settings.get('/', checkAuthenticated, (req, res) => {
     userInfo: req.user,
     page: 'settings'
   });
+});
+
+settings.post('/', checkAuthenticated, (req, res) => {
+  console.log(req.user);
+  console.log(req.body);
+  db.findOne({ _id: req.user._id }, (err, doc) => {
+    if (err) {
+      console.log(err);
+    } else {
+      db.update({ _id: req.user._id }, { $set: { userSettings: req.body } }, {}, function (err, numReplaced) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Number replaced: ');
+          console.log(numReplaced);
+        }
+      });
+    }
+  });
+  res.redirect({ message: 'success' });
 });
