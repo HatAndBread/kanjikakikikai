30a077a452467ca9f4a8640bbb03152df6f6ee77
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2020-08-11 16:58:37 +0900

    removed css, js files from service worker for debugging purposes

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,5 @@
 const kanji = 'kanjikakikikai';
 const assets = [
-  '/main.js',
-  '/style.css',
   '/assets/trash.png',
   '/assets/loader.gif',
   '/assets/keshi.png',
@@ -18,24 +16,7 @@ const assets = [
   '/word-sets/jlpt-four.jscsrc',
   '/word-sets/jlpt-three.jscsrc',
   '/word-sets/jlpt-two.jscsrc',
-  '/p5.min.js',
-  '/edit-sets-script.js',
-  'components/about.css',
-  'components/background.css',
-  'components/bottom.css',
-  'components/buttons.css',
-  'components/canvas.css',
-  'components/check-answer.css',
-  'components/create-new-set.css',
-  'components/editor-styles.css',
-  'components/forms.css',
-  'components/game-finished.css',
-  'components/greeting.css',
-  'components/hints.css',
-  'components/practice.css',
-  'components/range.css',
-  'components/select.css',
-  'components/settings.css'
+  '/p5.min.js'
 ];
 
 self.addEventListener('install', (installEvent) => {
